24678c0f61d5b8f89d59af0012e2b5418f7223bd
mikitro04 <m.tronu4@studenti.unica.it>
2024-10-15 09:34:04 +0200

    ciao

--- 1/maxnat/Max.ml
+++ 1/maxnat/Max.ml
@@ -1,6 +1,15 @@
 let max_nat a b = 
-  if (a > b) then
-    a
-  else 
-    b
+  if (a < 0 || b < 0) then
+    failwith "This function always fails"
+  else
+    if (a > b) then
+      a
+    else 
+      b
 ;;
+
+assert(max_nat 2 5 = 5);;
+assert(max_nat 5 2 = 5);;
+assert(try max_nat (-2) 5 |> fun _ -> false with _ -> true);;
+assert(try max_nat 2 (-5) |> fun _ -> false with _ -> true);;
+assert(try max_nat (-2) (-5) |> fun _ -> false with _ -> true);;
