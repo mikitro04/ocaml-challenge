4b3f2cf9aed58f76f5bbbdade4f821787ec27b3e
mikitro04 <m.tronu4@studenti.unica.it>
2024-11-05 16:47:23 +0100

    even

---
+++ 1/even-odd/even.ml
@@ -0,0 +1,16 @@
+let is_even (x : int) : bool = 
+  let even = x mod 2 in 
+  match even with
+  | 0 -> true
+  | _ -> false
+;;
+
+let win (a : int) (b : int) : int =
+  if (a >= 1 && a <= 5 && b >= 1 && b <= 5) then
+    if (is_even (a + b)) then 1
+    else -1
+  else
+    if (a >= 1 && a <= 5 && (b < 1 || b > 5)) then 1
+    else if (b >= 1 && b <= 5 && (a < 1 || a > 5)) then -1
+    else 0
+;;
